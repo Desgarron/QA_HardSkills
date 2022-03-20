41fce8720c6ecf99236e607cdd4615cdd5888b13
Yaroslav Nahornyi <95193486+Desgarron@users.noreply.github.com>
2022-03-20 13:38:25 +0200

    Update HomeWork_2.py

--- Python/HomeWork_2.py
+++ Python/HomeWork_2.py
@@ -170,10 +170,10 @@ b_item_relult_multi = b_item_t * b_item_f
 print('b_item_relult_multi = ', b_item_relult_multi)
 
 #  56. Создать переменную b_item_relult_division и присвоить деление b_item_t и b_item_f
-# b_item_relult_division = b_item_t / b_item_f
+b_item_relult_division = b_item_t / b_item_f
 
 #  57. Вывести b_item_relult_division в консоль. (Получить ошибку)
-# print('b_item_relult_division = ', b_item_relult_division)
+print('b_item_relult_division = ', b_item_relult_division)
 
 #  58. Создать переменную b_item_1_int и присвоить явное приведение b_item_t к int
 b_item_1_int = int(b_item_t)
